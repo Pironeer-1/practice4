8d8b5eb1c8a97168f4d8fbe78c0643ccc78a9508
ywonchae1 <ywonchae62@gmail.com>
2024-09-28 13:28:28 +0900

    fix: 중요! 수정, 삭제 작업에 Transactional 애노테이션 추가

--- src/main/java/com/pironeer/week4/topic/service/TopicService.java
+++ src/main/java/com/pironeer/week4/topic/service/TopicService.java
@@ -1,5 +1,6 @@
 package com.pironeer.week4.topic.service;
 
+import com.pironeer.week4.comment.repository.CommentRepositoryV3;
 import com.pironeer.week4.topic.dto.request.TopicCreateRequest;
 import com.pironeer.week4.topic.dto.request.TopicUpdateRequest;
 import com.pironeer.week4.topic.dto.response.TopicResponse;
@@ -8,6 +9,7 @@ import com.pironeer.week4.topic.mapper.TopicMapper;
 import com.pironeer.week4.comment.repository.CommentRepository;
 import com.pironeer.week4.topic.entity.Topic;
 import com.pironeer.week4.topic.repository.TopicRepositoryV3;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +20,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TopicService {
     private final TopicRepositoryV3 topicRepository;
-    private final CommentRepository commentRepository;
+    private final CommentRepositoryV3 commentRepository;
 
     public void save(TopicCreateRequest request) {
         topicRepository.save(TopicMapper.from(request));
@@ -50,6 +52,7 @@ public class TopicService {
         return TopicSliceResponse.of(topicResponses, cursorId, hasNext);
     }
 
+    @Transactional
     public TopicResponse update(TopicUpdateRequest request) {
         Topic topic = topicRepository.findById(request.id())
                 .orElseThrow(() -> new RuntimeException("TOPIC NOT FOUND"));
@@ -57,6 +60,7 @@ public class TopicService {
         return TopicResponse.of(topic);
     }
 
+    @Transactional
     public void deleteById(Long id) {
         commentRepository.deleteByTopicId(id);
         topicRepository.deleteById(id);
